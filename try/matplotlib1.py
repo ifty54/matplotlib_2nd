4b7f65c77c2df3f77bba1afa37530e6edd9249b2
Al-Amin Ifty <31790027+ifty54@users.noreply.github.com>
2023-03-05 00:43:21 +0600

    Update matplotlib1.py

--- try/matplotlib1.py
+++ try/matplotlib1.py
@@ -63,3 +63,54 @@ plt.plot(y, 'o:r', ms = 20, mfc='b') #where : shows dot, r is red
 #plt.plot(y, marker = 'o', ms = 20, mec = 'r', mfc = 'b')
 #mfc = marked face color; mec = marked edge color (HexaDec color, {specific name-color like hotpink} input is doable too)
 plt.show()
+
+
+#Scatter Plots
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([1,4,6,7,8,9,12,13,15,16])
+y = np.array([23,24,26,46,36,68,69,80,99,108])
+plt.scatter(x,y, color="yellow")
+
+x = np.array([2,3,5,6,7,8,9,14,15,47])
+y = np.array([21,25,26,35,46,49,76,77,88,98])
+plt.scatter(x,y, color= "red")
+
+plt.show()
+
+#separately color choice
+x = np.array([1,4,6,7,8,9,12,13,15,16])
+y = np.array([23,24,26,46,36,68,69,80,99,108])
+
+colors = np.array(["yellow","pink","black","orange","purple","beige","brown","gray","cyan","magenta"])
+sizes = np.array([120,300,600,700,81,41,100,390,800,90])
+plt.scatter(x,y, c=colors, cmap='nipy_spectral', s=sizes, alpha=0.5) #alpha measures opacity
+plt.title("Heat Map")
+plt.colorbar()
+plt.show()
+
+#bars
+x = ["Manchester United", "Manchester City", 'Chelsea']
+y = [400, 350, 500]
+plt.bar(x,y, color = 'red', width=0.1) #height is expected too
+#plt.barh(x,y) barh is horizontal
+plt.show()
+
+
+#histogram
+import numpy as np
+x = np.random.normal(180,20,70)
+
+plt.hist(x)
+plt.show()
+
+#pie chart
+
+y = np.array([20,25,30,40])
+mylabels = ["England", "France", "Portugal", "Uruguay"]
+mycolors = ["black", "hotpink", "b", "#4CAF50"]
+myexplode = [0.2,0.1,0,0] #explode makes distance
+plt.pie(y, labels = mylabels, startangle=290, explode = myexplode, shadow = True, colors=mycolors)
+plt.legend(title = "Countries")
+plt.show()
